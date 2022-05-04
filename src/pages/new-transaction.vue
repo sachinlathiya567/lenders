b18f37a7c326edc3de907e98da5ec95486fb78a5
EveSpiceapp <lathiyasachin567@gmail.com>
2022-05-04 15:54:24 +0300

    updated add transaction code

--- src/pages/new-transaction.vue
+++ src/pages/new-transaction.vue
@@ -1,10 +1,41 @@
 
 <script setup>
+import { ref } from "@vue/reactivity";
 import data from "../data.json";
 const user = localStorage.getItem("authUser");
 let authUser;
 if (user) {
   authUser = JSON.parse(user);
+}
+
+const users = data.users;
+
+const date = ref("");
+const amount = ref("");
+const type = ref("");
+const reason = ref("");
+const tuser = ref("");
+
+function handleSubmit() {
+  let transaction = {
+    id: 1235 + Math.floor(Math.random() * 100),
+    type: type.value,
+    amount: amount.value,
+    date: date.value,
+    reason: reason.value,
+    status: "unpaid",
+  };
+
+  if (type.value == "Lends") {
+      transaction.lender = authUser
+      transaction.borrower = tuser.value
+  } else {
+      transaction.borrower = authUser
+      transaction.lender = tuser.value
+  }
+
+  console.log("transaction", transaction);
+  alert("Success")
 }
 </script>
 
@@ -17,52 +48,64 @@ if (user) {
 
     <div class="row">
       <div class="col-12 col-sm-8 col-md-6">
-        <form>
+        <form @submit.prevent="handleSubmit">
           <div class="form-group">
-            <label for="exampleFormControlInput1">Email address</label>
+            <label for="exampleFormControlInput1">Date</label>
             <input
-              type="email"
+              v-model="date"
+              type="date"
               class="form-control"
               id="exampleFormControlInput1"
-              placeholder="name@example.com"
             />
           </div>
           <div class="form-group">
-            <label for="exampleFormControlSelect1">Example select</label>
-            <select class="form-control" id="exampleFormControlSelect1">
-              <option>1</option>
-              <option>2</option>
-              <option>3</option>
-              <option>4</option>
-              <option>5</option>
+            <label for="exampleFormControlInput1">Amount</label>
+            <input
+              v-model="amount"
+              type="number"
+              class="form-control"
+              id="exampleFormControlInput1"
+            />
+          </div>
+          <div class="form-group">
+            <label for="exampleFormControlSelect1">Type</label>
+            <select
+              v-model="type"
+              class="form-control"
+              id="exampleFormControlSelect1"
+            >
+              <option value="Borrows">Borrows</option>
+              <option value="Lends">Lends</option>
             </select>
           </div>
           <div class="form-group">
-            <label for="exampleFormControlSelect2"
-              >Example multiple select</label
+            <label for="exampleFormControlSelect2">
+              {{ type == "Lends" ? `to` : "" }}
+              {{ type == "Borrows" ? `from` : "" }}</label
             >
             <select
-              multiple
+              v-model="tuser"
               class="form-control"
               id="exampleFormControlSelect2"
             >
-              <option>1</option>
-              <option>2</option>
-              <option>3</option>
-              <option>4</option>
-              <option>5</option>
+              <option v-for="(user, index) in users" :key="index" :value="user">
+                {{ user?.name }}
+              </option>
             </select>
           </div>
           <div class="form-group">
-            <label for="exampleFormControlTextarea1">Example textarea</label>
+            <label for="exampleFormControlTextarea1"
+              >Reason for transaction</label
+            >
             <textarea
+              v-model="reason"
               class="form-control"
               id="exampleFormControlTextarea1"
               rows="3"
             ></textarea>
           </div>
 
-          <div class="btn btn-primary btn-lg" type="submit">Submit</div>
+          <button class="btn btn-primary btn-lg" type="submit">Submit</button>
         </form>
       </div>
     </div>
